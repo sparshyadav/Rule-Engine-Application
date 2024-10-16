f55a9b638091ea4cdf079874ad91b1ab61c5b9ac
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-10-16 14:00:52 +0530

    Create Basic Server Structure with Database

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dbConnect = require("./util/dbConnect.js");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+    dbConnect();
+})
+
