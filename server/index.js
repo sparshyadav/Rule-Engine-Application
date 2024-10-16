b84c69ff79f2ef7ff264a3ded3e5ad2db6072b83
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-10-16 14:38:44 +0530

    Complete index.js with Routes

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const dbConnect = require("./util/dbConnect.js");
+const rulesRoutes = require("./routes/rules.js");
 require("dotenv").config();
 
 const app = express();
@@ -9,6 +10,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
+
+app.use('/api/rules', rulesRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
